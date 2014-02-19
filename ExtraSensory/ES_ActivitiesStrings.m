912497e4681e13bb77da8377f548dd3f9cd221ed
Rafael Aguayo <raguayo@ucsd.edu>
2014-02-19 11:37:11 -0800

    Added class that returns array of strings for main activities, secondary activities, and moods.

---
+++ ExtraSensory/ES_ActivitiesStrings.m
@@ -0,0 +1,26 @@
+//
+//  ES_ActivitiesStrings.m
+//  ExtraSensory
+//
+//  Created by Rafael Aguayo on 2/18/14.
+//  Copyright (c) 2014 Bryan Grounds. All rights reserved.
+//
+
+#import "ES_ActivitiesStrings.h"
+
+@implementation ES_ActivitiesStrings
+
++(NSArray *)mainActivities {
+    
+    return @[@"Lying down", @"Sitting", @"Standing", @"Walking", @"Running", @"Bicycling"];
+}
+
++(NSArray *)associatedActivities {
+    return @[@"Lifting weights", @"Playing baseball", @"Playing basketball", @"Playing lacrosse", @"Skateboarding", @"Playing soccer", @"Playing frisbee", @"Stretching", @"Yoga", @"Elliptical machine", @"Treadmill", @"Stationary Bike", @"Cooking", @"Cleaning", @"Gardening", @"Doing laundry", @"Mowing the lawn", @"Raking the leaves", @"Vacuuming", @"Doing dishes", @"Washing car", @"Manual labor", @"Dancing", @"Driving", @"Eating", @"Drinking",@"Jumping", @"Listening to music", @"Relaxing", @"Shopping", @"Sleeping", @"Talking with friends", @"Using the bathroom", @"Playing videogames", @"Watching TV", @"Lab work", @"Written work", @"Drawing", @"Surfing the internet", @"Computer work", @"Reading a book", @"Studying", @"In class", @"In a meeting", @"Texting", @"At a bar", @"At a concert", @"At the beach", @"At a restaurant", @"On a bus", @"On a plane", @"On a train", @"In a car"];
+}
+
++(NSArray *)moods {
+    return @[@"Amused",@"Angry",@"Bored",@"Calm",@"Crazy",@"Disgusted",@"Dreamy",@"Energetic",@"Excited",@"Frustrated",@"Happy",@"High",@"Hungry",@"In love", @"Lonely", @"Normal", @"Nostalgic", @"Optimistic", @"Romantic", @"Sad", @"Serious", @"Sexy", @"Sleepy", @"Stressed", @"Tired", @"Untroubled", @"Worried"];
+}
+
+@end
