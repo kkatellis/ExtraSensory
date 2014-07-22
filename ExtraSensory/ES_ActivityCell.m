a60950eb7095a1b4fa8cd89aac3eca0698293834
Arya Iranmehr <arya@Aryas-Mac.local>
2014-07-22 12:08:25 -0700

    BugFix: Time indicator and activety title fixed in the calendar

--- ExtraSensory/ES_ActivityCell.m
+++ ExtraSensory/ES_ActivityCell.m
@@ -68,7 +68,7 @@
         }];
         
         [self.time makeConstraints:^(MASConstraintMaker *make) {
-            make.top.equalTo(self.title.bottom).offset(contentMargin-0.0);
+            make.top.equalTo(self.bottom).offset(contentMargin-17);
             make.left.equalTo(self.left).offset(contentPadding.left -33);
             make.right.equalTo(self.right).offset(-contentPadding.right);
             make.bottom.lessThanOrEqualTo(self.bottom).offset(-contentPadding.bottom );
