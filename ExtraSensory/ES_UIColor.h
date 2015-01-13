71252c7f4b48c137bd690c06e9f70592c79fbedf
Jenn Lu <jennlu@Jenns-Mac.local>
2015-01-12 20:18:18 -0800

    adding different colors to change color scheme

---
+++ ExtraSensory/ES_UIColor.h
@@ -0,0 +1,17 @@
+//
+//  ES_UIColor.h
+//  ExtraSensory
+//
+//  Created by Jenn Lu on 1/6/15.
+//  Copyright (c) 2015 Bryan Grounds. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ES_UIColor (CustomColors)
+
++ (UIColor *)myColorLightGreyBGColor;
+
+@end
+
+#endif
---
+++ ExtraSensory/ES_UIColor.m
@@ -0,0 +1,23 @@
+//
+//  ES_UIColor.m
+//  ExtraSensory
+//
+//  Created by Jenn Lu on 1/6/15.
+//  Copyright (c) 2015 Bryan Grounds. All rights reserved.
+//
+
+#import "UIColor+CustomColors.h"
+
+@interface UIColor (CustomColors)
+
++(UIColor *)paleYellowColor;
+
+@end
+
+@implementation UIColor (CustomColors)
+
++ (UIColor *)paleYellowColor {
+    return [UIColor colorWithHueDegrees:60 saturation:0.2 brightness:1.0];
+}
+
+@end
